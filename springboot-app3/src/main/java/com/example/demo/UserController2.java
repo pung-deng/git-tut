7a0ed5014bf22586ff05ac6b8348430dfc0600d5
pung-deng <ksg4883@naver.com>
2025-09-05 16:42:53 +0900

    hihi

--- springboot-app3/src/main/java/com/example/demo/UserController2.java
+++ springboot-app3/src/main/java/com/example/demo/UserController2.java
@@ -18,7 +18,7 @@ public class UserController2 {
 
     @GetMapping("/")
     public String hello() {
-        return "Spring Boot! STssdafddafasdfsART PAGE!!";
+        return "Spring Boot! STssdafdsdfsART PAGE!!";
     }
 
     @GetMapping("/mysql")
